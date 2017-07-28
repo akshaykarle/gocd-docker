66b3b4f1649175edf8e8289bf5b7876ce87dc9ac
Akshay Karle <akshay.a.karle@gmail.com>
2017-07-28 19:43:11 -0300

    add a sample go config with docker elastic agents

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@ set -e
 
 mkdir -p godata/plugins/external
 curl --location --fail https://github.com/gocd-contrib/docker-elastic-agents/releases/download/v0.7.0/docker-elastic-agents-0.7.0.jar > ./godata/plugins/external/docker-elastic-agents-0.7.0.jar
+mkdir -p godata/config
+cp goconfig/cruise-config.xml ./godata/config/cruise-config.xml

93a04bce6ec461bf8adccb1585293971edbac542
Akshay Karle <akshay.a.karle@gmail.com>
2017-07-28 15:08:54 -0300

    Initial go-cd setup with docker elastic agents plugin

---
+++ README.md
@@ -0,0 +1,7 @@
+# GoCD setup with docker
+Launch a GoCD server agent setup with a sample pipeline.
+
+## Getting started
+* Install `docker` and `docker-compose`
+* Run `./setup.sh`
+* Run `docker-compose up -d`
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+mkdir -p godata/plugins/external
+curl --location --fail https://github.com/gocd-contrib/docker-elastic-agents/releases/download/v0.7.0/docker-elastic-agents-0.7.0.jar > ./godata/plugins/external/docker-elastic-agents-0.7.0.jar

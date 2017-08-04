b62280f96462c46fc3524b7437761fd6c97935a9
Akshay Karle <akshay.a.karle@gmail.com>
2017-08-04 20:39:08 -0300

    add github oauth plugin

--- setup.sh
+++ setup.sh
@@ -11,3 +11,6 @@ fi
 if [ ! -f ./godata/plugins/external/yaml-config-plugin-0.4.0.jar ]; then
   curl --location --fail https://github.com/tomzo/gocd-yaml-config-plugin/releases/download/0.4.0/yaml-config-plugin-0.4.0.jar > ./godata/plugins/external/yaml-config-plugin-0.4.0.jar
 fi
+if [ ! -f ./godata/plugins/external/github-oauth-authorization-plugin-1.0.0-1.jar ]; then
+  curl --location --fail https://github.com/gocd-contrib/github-oauth-authorization-plugin/releases/download/1.0.0/github-oauth-authorization-plugin-1.0.0-1.jar > ./godata/plugins/external/github-oauth-authorization-plugin-1.0.0-1.jar
+fi
